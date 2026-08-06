b540c5a597c1a1879fd12da458141712effb7472
sytong2002 <sytong2002@gmail.com>
2026-08-07 02:56:51 +0700

    Update Log.java

--- src/main/java/com/nqsh/common/Log.java
+++ src/main/java/com/nqsh/common/Log.java
@@ -1,1 +1,16 @@
+package com.nqsh.common;
 
+public final class Log {
+
+    public static void info(String text) {
+        System.out.println("[INFO] " + text);
+    }
+
+    public static void warn(String text) {
+        System.out.println("[WARN] " + text);
+    }
+
+    public static void error(String text) {
+        System.err.println("[ERROR] " + text);
+    }
+}
