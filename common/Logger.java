0e34261decc2b5c74f579a924d220544b1f9eac5
sytong2002 <sytong2002@gmail.com>
2026-08-07 02:45:14 +0700

    Update Logger.java

--- common/Logger.java
+++ common/Logger.java
@@ -1,1 +1,15 @@
+public final class Logger {
 
+    public static void info(String s){
+        System.out.println("[INFO] " + s);
+    }
+
+    public static void warn(String s){
+        System.out.println("[WARN] " + s);
+    }
+
+    public static void error(String s){
+        System.err.println("[ERROR] " + s);
+    }
+
+}
