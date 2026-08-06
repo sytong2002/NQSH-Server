0dcae51820a2d56399f9a524289691495e990632
sytong2002 <sytong2002@gmail.com>
2026-08-07 02:53:22 +0700

    Update Config.java

--- src/main/java/com/nqsh/common/Config.java
+++ src/main/java/com/nqsh/common/Config.java
@@ -1,1 +1,13 @@
+package com.nqsh.common;
 
+public final class Config {
+
+    public static final String HOST = "0.0.0.0";
+    public static final int PORT = 14444;
+
+    public static final String DB_HOST = "127.0.0.1";
+    public static final int DB_PORT = 3306;
+    public static final String DB_NAME = "nqsh";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "";
+}
